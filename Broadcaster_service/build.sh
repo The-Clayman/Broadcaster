4172d549769ae9ac30a74cee836b34e3a36a11da
Roy <zurroy@gmail.com>
2023-10-23 23:14:23 +0300

    Add docker run commands examples

--- Broadcaster_service/build.sh
+++ Broadcaster_service/build.sh
@@ -1,4 +1,9 @@
 sudo docker build -t broadcaster_service .
 
 # Container run command example:
+# 1. RTSP server container command example:
+#sudo docker run -d --rm --name rtsp_server --network=host aler9/rtsp-simple-server
+
+# 2. Run Broadcaster application container:
 # sudo docker run -d --name broadcaster --network=host broadcaster_service
+
