abdfec28f7b166fa733bda88d5e226f2b81f815e
Roy <zurroy@gmail.com>
2024-02-01 14:29:33 +0200

    Add video table component

---
+++ Broadcaster_frontend/src/app/components/video-table/video-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-table',
+  templateUrl: './video-table.component.html',
+  styleUrl: './video-table.component.css'
+})
+export class VideoTableComponent {
+
+}
