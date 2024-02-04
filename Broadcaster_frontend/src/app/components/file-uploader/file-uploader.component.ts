59b251fc66fdec8fe065bbc382626e646468bc58
Roy <zurroy@gmail.com>
2024-02-04 10:51:47 +0200

    Roll back to single upload button

--- Broadcaster_frontend/src/app/components/file-uploader/file-uploader.component.ts
+++ Broadcaster_frontend/src/app/components/file-uploader/file-uploader.component.ts
@@ -9,14 +9,15 @@ import { ToasterService } from '../../services/toaster.service';
   styleUrl: './file-uploader.component.css'
 })
 export class FileUploaderComponent {
-  fileName: string = "";
-  private fileData:any = null;
+
+  private fileName:string  = '';
 
   constructor(private broadcasterService: BraodcasterService , private toasterService:ToasterService){}
 
   onFileSelected(event: any) { 
 
     const file:File = event.target.files[0];
+
 
     if (file) {
 
@@ -25,24 +26,15 @@ export class FileUploaderComponent {
         const formData = new FormData();
 
         formData.append("file", file);
-        this.fileData = formData;
-    }
-  }
 
-  onUploadClicked(){
-    if (this.fileData != null){
-      const upload$ = this.broadcasterService.uploadFile(this.fileData);
+        const upload$ = this.broadcasterService.uploadFile(formData);
 
-      upload$.subscribe(
-        res => {console.log('HTTP response', res); this.toasterService.popToasterMessage("video "+ this.fileName+ " uploaded", "regular")},
-        err => {console.log('HTTP Error', err); this.toasterService.popToasterMessage('Failed to upload video ['+ err.status+"],\n" + err.error.message, "error");},
-        () => {console.log('HTTP response'); this.toasterService.popToasterMessage("video "+ this.fileName+ " uploaded", "regular")}
+        upload$.subscribe(
+          res => {console.log('HTTP response', res); this.toasterService.popToasterMessage("video "+ this.fileName+ " uploaded", "regular")},
+          err => {console.log('HTTP Error', err); this.toasterService.popToasterMessage('Failed to upload video ['+ err.status+"],\n" + err.error.message, "error");},
+          () => {console.log('HTTP response'); this.toasterService.popToasterMessage("video "+ this.fileName+ " uploaded", "regular")}
       );
-
-    } else {
-      this.toasterService.popToasterMessage("No file were selected to upload", "error");
-    } 
-
+    }
   }
 
 }
