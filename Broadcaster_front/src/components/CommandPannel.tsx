b728e3e76a1545c0df780ff1a64384daef3acbe2
Roy <zurroy@gmail.com>
2024-02-16 19:27:23 +0200

    #11 add error toaster on no selected videos

--- Broadcaster_front/src/components/CommandPannel.tsx
+++ Broadcaster_front/src/components/CommandPannel.tsx
@@ -11,64 +11,75 @@ export interface Props {
 const CommandPannel = ({baseUrl, selectedRows, popToast}: Props) => {
 
     const playVideo = (selectedRows: any[]) => {
-        selectedRows.forEach(videoName => {
-            axios.post(`${baseUrl}/broadcaster/v1/player/${videoName}/play`)
-            .then(res => {
-             if (res.status === 200){
-               let toasterMessage = res.data.message != null ? res.data.message : "sent play successufly";
-               popToast(toasterMessage, "success");
-             } else {
-               let toasterMessage = res.data.message != null ? res.data.message : "send play failed";
-               popToast(toasterMessage, "error");
-             } 
-           })
-           .catch((error) => {
-             console.log(error);
-             let errorMessage = error.response.data.message != null ? error.response.data.message : "failed to send play";
-             popToast(errorMessage, "error");
-           });
-        });;
-
+        if (selectedRows.length > 0) {
+            selectedRows.forEach(videoName => {
+                axios.post(`${baseUrl}/broadcaster/v1/player/${videoName}/play`)
+                .then(res => {
+                if (res.status === 200){
+                let toasterMessage = res.data.message != null ? res.data.message : "sent play successufly";
+                popToast(toasterMessage, "success");
+                } else {
+                let toasterMessage = res.data.message != null ? res.data.message : "send play failed";
+                popToast(toasterMessage, "error");
+                } 
+            })
+            .catch((error) => {
+                console.log(error);
+                let errorMessage = error.response.data.message != null ? error.response.data.message : "failed to send play";
+                popToast(errorMessage, "error");
+            });`d`
+            });;
+        } else {
+            popToast("no video were selected", "error");
+        }
     }
 
     const stopVideo = (selectedRows: any[]) => {
-        selectedRows.forEach(videoName => {
-            axios.post(`${baseUrl}/broadcaster/v1/player/${videoName}/stop`)
-            .then(res => {
-             if (res.status === 200){
-               let toasterMessage = res.data.message != null ? res.data.message : "sent stop successufly";
-               popToast(toasterMessage, "success");
-             } else {
-               let toasterMessage = res.data.message != null ? res.data.message : "send stop failed";
-               popToast(toasterMessage, "error");
-             } 
-           })
-           .catch((error) => {
-             console.log(error);
-             let errorMessage = error.response.data.message != null ? error.response.data.message : "failed to send stop";
-             popToast(errorMessage, "error");
-           });
-        });;
+        if (selectedRows.length > 0) {
+            selectedRows.forEach(videoName => {
+                axios.post(`${baseUrl}/broadcaster/v1/player/${videoName}/stop`)
+                .then(res => {
+                if (res.status === 200){
+                let toasterMessage = res.data.message != null ? res.data.message : "sent stop successufly";
+                popToast(toasterMessage, "success");
+                } else {
+                let toasterMessage = res.data.message != null ? res.data.message : "send stop failed";
+                popToast(toasterMessage, "error");
+                } 
+            })
+            .catch((error) => {
+                console.log(error);
+                let errorMessage = error.response.data.message != null ? error.response.data.message : "failed to send stop";
+                popToast(errorMessage, "error");
+            });
+            });;
+        } else {
+            popToast("no video were selected", "error");
+        }
     }
 
     const deleteVideo = (selectedRows: any[]) => {
-        selectedRows.forEach(videoName => {
-            axios.delete(`${baseUrl}/broadcaster/v1/videos/${videoName}`)
-            .then(res => {
-             if (res.status === 200){
-               let toasterMessage = res.data.message != null ? res.data.message : "sent stop successufly";
-               popToast(toasterMessage, "success");
-             } else {
-               let toasterMessage = res.data.message != null ? res.data.message : "send stop failed";
-               popToast(toasterMessage, "error");
-             } 
-           })
-           .catch((error) => {
-             console.log(error);
-             let errorMessage = error.response.data.message != null ? error.response.data.message : "failed to send stop";
-             popToast(errorMessage, "error");
-           });
-        });;
+        if (selectedRows.length > 0) {
+            selectedRows.forEach(videoName => {
+                axios.delete(`${baseUrl}/broadcaster/v1/videos/${videoName}`)
+                .then(res => {
+                if (res.status === 200){
+                let toasterMessage = res.data.message != null ? res.data.message : "sent stop successufly";
+                popToast(toasterMessage, "success");
+                } else {
+                let toasterMessage = res.data.message != null ? res.data.message : "send stop failed";
+                popToast(toasterMessage, "error");
+                } 
+            })
+            .catch((error) => {
+                console.log(error);
+                let errorMessage = error.response.data.message != null ? error.response.data.message : "failed to send stop";
+                popToast(errorMessage, "error");
+            });
+            });;
+        } else {
+            popToast("no video were selected", "error");
+        }
     }
         
 
