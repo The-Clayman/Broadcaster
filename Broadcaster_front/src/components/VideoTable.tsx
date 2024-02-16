732c443912244e6e664a0f74a3697efbb1756d2f
Roy <zurroy@gmail.com>
2024-02-16 10:48:12 +0200

    #11 create basic VideoTable under components
    Add bootstrap styling

--- Broadcaster_front/src/components/VideoTable.tsx
+++ Broadcaster_front/src/components/VideoTable.tsx
@@ -1,7 +1,27 @@
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React from "react";
 
 
+const dummyRows = [
+    { video_name: 'Norway', status: 'RUNNING', rtsp_url: 'rtsp://192.168.1.100:5'},
+    { video_name: 'France', status: 'RAEDY', rtsp_url: null},
+    { video_name: 'England', status: 'FAILED_TRANSCODING'},
+];
+
+const columns: GridColDef[] = [
+    { field: 'video_name', headerName: 'Video name', width: 600 },
+    { field: 'status',headerName: 'Status', width: 600},
+    { field: 'rtsp_url', headerName: 'Rtsp', width: 600}
+  ];
+
+
 export default function VideoTable() {
-    return <div>VideoTable element</div>;
+    return <div>VideoTable element
+        <DataGrid
+            rows={dummyRows}
+            columns={columns}
+            getRowId={(row) => row.video_name}
+        />
+    </div>;
 }
 
